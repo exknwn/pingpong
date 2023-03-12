384e7881813db803c198b230fedba519657b147e
exknwn <127669043+exknwn@users.noreply.github.com>
2023-03-12 18:50:04 +0500

    Update pp.py
    +file +object

--- pp.py
+++ pp.py
@@ -1,8 +1,8 @@
 from pygame import *
 
-sc_width = 700
-sc_height = 500
-scene = display.set_mode(sc_width, sc_height)
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -16,30 +16,41 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update_r(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.y < sc_width - 80:
-            self.rect.x += self.speed
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys[K_s] and self.rect.y < sc_width - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 1:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 350:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 1:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 350:
+            self.rect.y += self.speed
 
+racket1 = Player('racket.png', 30, 200, 50, 150, 4)
+racket2 = Player('racket.png', 620, 200, 50, 150, 4)
+ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 4)
 
 clock = time.Clock()
 FPS = 60
 
 game = True
+finish = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-scene.fill(214, 174, 211)
+    if finish == False:
+        racket1.update_l()
+        racket2.update_r()
 
-display.update()
-clock.tick(FPS)
+        window.fill((214, 174, 211))
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
