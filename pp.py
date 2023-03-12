372dd8950c672567225dd1f74d3bfeaefa1363fc
exknwn <127669043+exknwn@users.noreply.github.com>
2023-03-12 18:29:23 +0500

    Update pp.py
    +gamesprite +class player

--- pp.py
+++ pp.py
@@ -4,7 +4,30 @@ sc_width = 700
 sc_height = 500
 scene = display.set_mode(sc_width, sc_height)
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image),(size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.y < sc_width - 80:
+            self.rect.x += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.y < sc_width - 80:
+            self.rect.x += self.speed
 
 
 clock = time.Clock()
@@ -15,8 +38,8 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-            
-window.fill(214, 174, 211)
+
+scene.fill(214, 174, 211)
 
 display.update()
 clock.tick(FPS)
